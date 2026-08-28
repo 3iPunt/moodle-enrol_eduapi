deab74faf7a4dce65245b064abf39811e862a6c2
Antoni Bertran <antoni@tresipunt.com>
2026-08-28 10:30:45 +0200

    docs(lang): point root_url help to the discovery servers URL
    Testing against the 1EdTech conformance server showed that entering the
    discovery hostname as root_url yields HTTP 500 on every resource endpoint.
    Clarify in en/ca/es that the value must be the 'servers' URL from the
    provider's OpenAPI discovery document, and add a Greek (el) pack with the
    root_url label and description.

---
+++ lang/el/enrol_eduapi.php
@@ -0,0 +1,27 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'enrol_eduapi', language 'el'.
+ *
+ * @package    enrol_eduapi
+ * @copyright  2026 3iPunt (contacte@tresipunt.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+$string['settings_connection_root_url'] = 'Ριζική διεύθυνση URL του Edu-API';
+$string['settings_connection_root_url_desc'] = 'Η ριζική διεύθυνση URL του Edu-API v1p0 του παρόχου, συμπεριλαμβανομένης οποιασδήποτε διαδρομής ειδικής για τον πάροχο. Χρησιμοποιήστε τη διεύθυνση URL \'servers\' που δηλώνεται στο έγγραφο ανακάλυψης OpenAPI του παρόχου (για παράδειγμα https://example.org/ims/eduapi/base/v1p0), όχι το όνομα κεντρικού υπολογιστή του ίδιου του εγγράφου ανακάλυψης. Χρησιμοποιείται ακριβώς όπως εισάγεται: δεν προστίθεται αυτόματα καμία διαδρομή.';
